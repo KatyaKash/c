b34a65d3b54edc3452037b1d225a04cb8fc19904
Bruhskiy <bruhskiymeme@gmail.com>
2022-11-11 11:18:22 +0300

    second

--- huh.c
+++ huh.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 int main(){
-    int x, y, z;
+    int x, y, z, d;
 
-    printf("Введите x: ");
+    printf("Vvedite x: ");
     scanf("%d", &x);
-    printf("Введите y: ");
+    printf("Vvedite y: ");
     scanf("%d", &y);
-    z = x + y;
-    printf("%d + %d = %d\n", x, y, z);
-	return 0;
+    printf("Viberite deystvie:\n");
+    printf("1 - slojenie\n");
+    printf("2 - vichitanie\n");
+    printf("3 - umnojenie\n");
+    printf("4 - delenie\n");
+    scanf("%d", &d);
+
+    if (d == 1) {
+        z = x + y;
+        printf("%d + %d = %d\n", x, y, z);
+    }
+    else if (d == 2) {
+        z = x - y;
+        printf("%d - %d = %d\n", x, y, z);
+    }
+    else if (d == 3) {
+        z = x * y;
+        printf("%d * %d = %d\n", x, y, z);
+    }
+    else if (d == 4) {
+        if (y == 0) {
+            printf("V Afrike poshel sneg");
+        }
+        else {
+            z = x / y;
+            printf("%d / %d = %d\n", x, y, z);
+        }
+    }    
+    else {
+        printf("Neverniy vvod\n");
+    }
+    return 0;
 }
